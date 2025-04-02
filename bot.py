68028261664b6de9d61d062595dbc3dffe82814e
Kenjomo <134088741+kenjomo@users.noreply.github.com>
2025-04-02 19:53:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,10 @@
-# Вместо your_token поставьте свой токен // instead of your_token, stake your token
+import telebot
 
-import telebot  # импортируем telebot // importing telebot
+token = ('your_token')  # instead of "your_token", stake your token
+bot = telebot.TeleBot(token)
 
-token = ('your_token')  # сохраняем токен бота // save the bot token
-bot = telebot.TeleBot(token)  # создаем бота // сreating a bot
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    bot.send_message(message.chat.id, message.text)
 
-@bot.message_handler(content_types=['text']) # хэндлер, который срабатывает на текст // handler that triggers on text
-def echo(message): # объявляем функцию "echo" // announcing the "echo" function
-    bot.send_message(message.chat.id, message.text)  # повторяем сообщение юзера // repeating the user's message
-
-bot.infinity_polling() # long polling
+bot.infinity_polling()
